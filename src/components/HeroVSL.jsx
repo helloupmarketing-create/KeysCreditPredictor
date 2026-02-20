fc9131952251014d02cab60caaaea33d294fa601
eric <eric@erics-MacBook-Pro.local>
2026-02-20 13:40:30 -0700

    fix: apply super-aggressive cropping (150%) for mobile YouTube branding

--- src/components/HeroVSL.jsx
+++ src/components/HeroVSL.jsx
@@ -149,17 +149,17 @@ const HeroVSL = ({ onStart }) => {
                 }}>
                     <iframe
                         ref={iframeRef}
-                        width="120%"
-                        height="120%"
-                        src="https://www.youtube.com/embed/_Q0eE7MsKKY?controls=0&amp;rel=0&amp;modestbranding=1&amp;enablejsapi=1&amp;iv_load_policy=3&amp;fs=0&amp;disablekb=1&amp;showinfo=0"
+                        width="150%"
+                        height="150%"
+                        src="https://www.youtube.com/embed/_Q0eE7MsKKY?controls=0&amp;rel=0&amp;modestbranding=1&amp;enablejsapi=1&amp;iv_load_policy=3&amp;fs=0&amp;disablekb=1&amp;showinfo=0&amp;playsinline=1"
                         title="YouTube VSL"
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         referrerPolicy="strict-origin-when-cross-origin"
                         style={{
                             position: 'absolute',
-                            top: '-10%',
-                            left: '-10%',
+                            top: '-25%',
+                            left: '-25%',
                             pointerEvents: 'none'
                         }}
                     ></iframe>
